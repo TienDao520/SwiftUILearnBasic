6c9117375f2914c37274993f12d0b5b7e3b8bb1f
Tien Dao <tiendao5240@gmail.com>
2022-11-21 12:07:26 -0500

    SwiftUILearnBasic: first commit

---
+++ SwiftUILearnBasic/SwiftUILearnBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearnBasicApp.swift
+//  SwiftUILearnBasic
+//
+//  Created by Tien Dao on 2022-11-21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearnBasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
